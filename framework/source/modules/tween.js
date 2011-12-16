84f0222131eede5e53c95523ad9b3713bd8e294a
Tyler Beck <tyler@tylerbeck.com>
2011-12-16 11:28:43 -0500

    -dev

--- framework/source/modules/tween.js
+++ framework/source/modules/tween.js
@@ -1,16 +1,15 @@
 (function( lola ) {
 	var $ = lola;
 	/**
-	 * Easing Module
+	 * Tween Module
 	 * @implements {lola.Module}
 	 * @memberof lola
 	 */
-	var easing = {
+	var tween = {
 
 		//==================================================================
 		// Attributes
 		//==================================================================
-
 
 
 		//==================================================================
@@ -22,14 +21,29 @@
 		 * @return {void}
 		 */
 		preinitialize: function() {
-			lola.debug( 'lola.easing::preinitialize' );
+			lola.debug('lola.tween::preinitialize');
 			if ( !lola ) throw new Error( 'lola not defined!' );
 
 			//do module preinitialization
-
+            if ( window.requestAnimationFrame ) {
+                lola.tween.requestTick = function(){ lola.window.requestAnimationFrame( lola.tween.tick ); };
+                lola.tween.requestFrame = function( callback ){ lola.window.requestAnimationFrame( callback ); };
+            }
+            if ( window.mozRequestAnimationFrame ){
+                lola.tween.requestTick = function(){ lola.window.mozRequestAnimationFrame( lola.tween.tick ); };
+                lola.tween.requestFrame = function( callback ){ lola.window.mozRequestAnimationFrame( callback ); };
+            }
+            else if ( window.webkitRequestAnimationFrame ){
+                lola.tween.requestTick = function(){ lola.window.webkitRequestAnimationFrame( lola.tween.tick ); };
+                lola.tween.requestFrame = function( callback ){ lola.window.webkitRequestAnimationFrame( callback ); };
+            }
+            else if ( window.oRequestAnimationFrame ){
+                lola.tween.requestTick = function(){ lola.window.oRequestAnimationFrame( lola.tween.tick ); };
+                lola.tween.requestFrame = function( callback ){ lola.window.oRequestAnimationFrame( callback ); };
+            }
 
 			//remove initialization method
-			delete lola.easing.preinitialize;
+			delete lola.tween.preinitialize;
 		},
 
 		/**
@@ -38,15 +52,16 @@
 		 * @return {void}
 		 */
 		initialize: function() {
-			lola.debug( 'lola.easing::initialize' );
+			lola.debug('lola.tween::initialize');
 			//this framework is dependent on lola framework
 			if ( !lola ) throw new Error( 'lola not defined!' );
 
 			//do module initialization
 
 
+
 			//remove initialization method
-			delete lola.easing.initialize;
+			delete lola.tween.initialize;
 		},
 
 		/**
@@ -55,7 +70,7 @@
 		 * @return {String}
 		 */
 		getNamespace: function() {
-			return "easing";
+			return "tween";
 		},
 
 		/**
@@ -65,31 +80,24 @@
 		 * @default []
 		 */
 		getDependencies: function() {
-			return ["math.point","graphics"];
+			return [];
 		},
 
+        /**
+         * set callback for animation frame
+         * @private
+         */
+        requestTick: function(){
+            setTimeout( lola.tween.tick, lola.tween.tickDelay );
+        },
 
-		/**
-		 * calculates a point on a cubic bezier curve given time and an array of points.
-		 * @private
-		 * @param {Number} t time 0 <= t <= 1
-		 * @param {lola.graphics.Point} p0 anchor 1
-		 * @param {lola.graphics.Point} p1 control 1
-		 * @param {lola.graphics.Point} p2 control 2
-		 * @param {lola.graphics.Point} p3 anchor 2
-		 * @return {lola.graphics.Point}
-		 */
-		cubicBezier: function( t, p0, p1, p2, p3 ) {
-			var inv = 1 - t;
-			return lola.math.point.add(
-					lola.math.point.multiply( p0, inv * inv * inv ),
-					lola.math.point.multiply( p1, 3 * inv * inv * t ),
-					lola.math.point.multiply( p2, 3 * inv * t * t ),
-					lola.math.point.multiply( p3, t * t * t )
-			);
-
-		},
-
+        /**
+         * set callback for animation frame
+         * @private
+         */
+        requestFrame: function(){
+            setTimeout( lola.tween.tick, lola.tween.tickDelay );
+        },
 
 
 		//==================================================================
@@ -126,8 +134,10 @@
 	//==================================================================
 
 
+
+
 	//register module
-	lola.registerModule( easing );
+	lola.registerModule( tween );
 
 })( lola );
 
